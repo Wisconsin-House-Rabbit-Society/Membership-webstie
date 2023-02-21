37e228eb6a5d8b3ccd01906004acc1ff4c518245
Marvin <marvin.tan@wisc.edu>
2023-02-20 19:58:20 -0600

    fix typo

--- member_info/models.py
+++ member_info/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    aleternative_first_name = models.CharField(max_length=150, blank=True)
-    aleternative_last_name = models.CharField(max_length=150, blank=True)
+    alternative_first_name = models.CharField(max_length=150, blank=True)
+    alternative_last_name = models.CharField(max_length=150, blank=True)
